f4d5ef3940664333d766d51a891529c0343fb8d0
Maciej Toborek <maciej@toborek.com>
2024-06-16 20:06:28 +0100

    fetch stacks

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+const sb = useSupabaseClient()
+
+  const stacks = await sb.from('stacks').select()
 </script>
 
 <template>
   <div class="py-8 px-5">
     <h3 class="text-xl font-semibold">Cube stacks</h3>
 
-
+    <p v-for="stack in stacks.data" :key="stack.id">{{ stack.name }}</p>
   </div>
 </template>
